25314e92d5a45a1f612f89ac398bb770670b9d98
kaffeewolf <kaffeewolf@users.noreply.github.com>
2022-07-21 18:50:41 +0000

    deploy: gscept/nebula@327c08b51e2a267827908cdddd31833b5ae7928e

--- struct_terrain_1_1_terrain_setup_settings.js
+++ struct_terrain_1_1_terrain_setup_settings.js
@@ -5,8 +5,8 @@ var struct_terrain_1_1_terrain_setup_settings =
     [ "quadsPerTileX", "struct_terrain_1_1_terrain_setup_settings.html#ab21fcf9da43992f0e5a7e899b3a7d880", null ],
     [ "quadsPerTileY", "struct_terrain_1_1_terrain_setup_settings.html#a5e47b5daa386a6527014f042737ce9b3", null ],
     [ "sun", "struct_terrain_1_1_terrain_setup_settings.html#a56708a575772c7d29125fc7fd65d7eba", null ],
-    [ "tileHeight", "struct_terrain_1_1_terrain_setup_settings.html#a82b39350c8e1de39a62b5c2ae59dbb0f", null ],
-    [ "tileWidth", "struct_terrain_1_1_terrain_setup_settings.html#a71da080da2a652d6f79d3eb26ac394f5", null ],
+    [ "tileHeight", "struct_terrain_1_1_terrain_setup_settings.html#a04b8872e9756e8e0f4670b23ef7c2d48", null ],
+    [ "tileWidth", "struct_terrain_1_1_terrain_setup_settings.html#ada7d3404394df309ed09a33dcbe3bce3", null ],
     [ "worldSizeX", "struct_terrain_1_1_terrain_setup_settings.html#a9c639696d58e8d333e97b642900bdb76", null ],
     [ "worldSizeZ", "struct_terrain_1_1_terrain_setup_settings.html#aa7fcc989fb5d5f9ba872b1fb3c372b3e", null ]
 ];
