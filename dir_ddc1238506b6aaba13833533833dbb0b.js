9a93cbcaece755e0ce5a911265584cbb48c5fe69
Duttenheim <Duttenheim@users.noreply.github.com>
2022-09-17 16:11:45 +0000

    deploy: gscept/nebula@fc989a23b3180dc29c86cf7ec54a35e0ac9175df

--- dir_ddc1238506b6aaba13833533833dbb0b.js
+++ dir_ddc1238506b6aaba13833533833dbb0b.js
@@ -5,7 +5,7 @@ var dir_ddc1238506b6aaba13833533833dbb0b =
     [ "win32criticalsection.h", "win32criticalsection_8h.html", "win32criticalsection_8h" ],
     [ "win32event.h", "win32event_8h.html", "win32event_8h" ],
     [ "win32interlocked.h", "win32interlocked_8h.html", "win32interlocked_8h" ],
-    [ "win32readwritelock.cc", "win32readwritelock_8cc.html", null ],
+    [ "win32readwritelock.cc", "win32readwritelock_8cc.html", "win32readwritelock_8cc" ],
     [ "win32readwritelock.h", "win32readwritelock_8h.html", "win32readwritelock_8h" ],
     [ "win32thread.cc", "win32thread_8cc.html", "win32thread_8cc" ],
     [ "win32thread.h", "win32thread_8h.html", "win32thread_8h" ],
---
+++ win32readwritelock_8cc.js
@@ -0,0 +1,5 @@
+var win32readwritelock_8cc =
+[
+    [ "readCounter", "win32readwritelock_8cc.html#a5c4baf2cd1a6cc1f50e3be6c19d9b5fe", null ],
+    [ "writeCounter", "win32readwritelock_8cc.html#ab3b392be0f187e64130a068fcd82fecb", null ]
+];
