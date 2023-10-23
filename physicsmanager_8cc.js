9cfb77dd90f87efb15e6a6382333b7cfdb8fd4c9
fLindahl <fLindahl@users.noreply.github.com>
2023-10-23 13:59:21 +0000

    deploy: gscept/nebula@931a1fa37fa830944516468470cc8e1afba7d96b

--- physicsmanager_8cc.js
+++ physicsmanager_8cc.js
@@ -5,6 +5,6 @@ var physicsmanager_8cc =
     [ "JsonReader::Get< Physics::ActorType >", "physicsmanager_8cc.html#a35739b15343f9119df4580b4504eba97", null ],
     [ "JsonWriter::Add< Physics::ActorId >", "physicsmanager_8cc.html#a187502a5699069a989bd0bebb42c1761", null ],
     [ "JsonWriter::Add< Physics::ActorType >", "physicsmanager_8cc.html#ab17f0915cc26f37c426b3227199bf4f1", null ],
-    [ "PassKinematicTransforms", "physicsmanager_8cc.html#a9dcfacf52601730dd30a0394cfd496e5", null ],
-    [ "PollRigidbodyTransforms", "physicsmanager_8cc.html#aee08253ec15682d97304cd1579acb0f1", null ]
+    [ "PassKinematicTransforms", "physicsmanager_8cc.html#a199fa71684584bb1d82732a6b75f7214", null ],
+    [ "PollRigidbodyTransforms", "physicsmanager_8cc.html#ac8d1c3202e0e8867d0e96b08f8c5188e", null ]
 ];
