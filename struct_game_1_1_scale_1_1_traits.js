6c4f6201584cbdfe73e0c2e1b021d3630fb6533d
fLindahl <fLindahl@users.noreply.github.com>
2024-02-28 22:25:50 +0000

    deploy: gscept/nebula@1ad16ea24bc4268ed9f60465b242b8c22fabf455

--- struct_game_1_1_scale_1_1_traits.js
+++ struct_game_1_1_scale_1_1_traits.js
@@ -3,6 +3,7 @@ var struct_game_1_1_scale_1_1_traits =
     [ "type", "struct_game_1_1_scale_1_1_traits.html#a0bf035f85cf7ba733e09195adbe6408d", null ],
     [ "Traits", "struct_game_1_1_scale_1_1_traits.html#a350a2a11deae123a1900448e4d236a18", null ],
     [ "field_names", "struct_game_1_1_scale_1_1_traits.html#a55851dac0c79f2640401609eb082b213", null ],
+    [ "fixed_column_index", "struct_game_1_1_scale_1_1_traits.html#ae21f6d510079a0686936045866a6ff0d", null ],
     [ "fully_qualified_name", "struct_game_1_1_scale_1_1_traits.html#a07207eb3ecf007a1b3772dd30c7c0e90", null ],
     [ "name", "struct_game_1_1_scale_1_1_traits.html#a7783ac100437b62d6efcd8384c27f2f5", null ],
     [ "num_fields", "struct_game_1_1_scale_1_1_traits.html#aab9cf316b1132356863dcddc8a2bc084", null ]
